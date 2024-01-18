d81c4fab8dcbbf389afe26dccb78aa44198b0827
NikolausA <nik.aleksenko@gmail.com>
2024-01-18 23:24:51 +0300

    create todo list from json placeholder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
+import trash from './icons8-trash-30.png';
 
 export const App = () => {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
+			.then((loadedData) => loadedData.json())
+			.then((loadedTodos) => {
+				setTodos(loadedTodos);
+			});
+	}, []);
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload. Here we go!
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className={styles.app}>
+			<h1 className={styles.title}>Todo List (Список задач)</h1>
+			<div className={styles.titleLine}>
+				<div>#</div>
+				<div className={styles.todosTitle}>Todos Name</div>
+			</div>
+			<div className={styles.todosWrapper}>
+				{todos.map(({ id, title }) => (
+					<div key={id} className={styles.item}>
+						<p className={styles.itemText}>
+							{id}. {title}
+						</p>
+						<img src={trash} alt="trash" className={styles.trash} />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
